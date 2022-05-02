231ae120b3c2763b9d9a7c554be18daf0653f7e6
Maxime Ellerbach <maxime@ellerbach.net>
2022-05-02 23:27:38 +0200

    working fisheye camera saving texture to file

---
+++ autocopylot/Assets/Scripts/CameraSensor.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections;
+using UnityEngine;
+
+public class CameraSensor : MonoBehaviour
+{
+    public RenderTexture renderTexture;
+
+    Texture2D tex;
+
+
+    Texture2D GetImage()
+    {
+        var currentRT = RenderTexture.active;
+        RenderTexture.active = renderTexture;
+
+        int texwidth = renderTexture.width;
+        int texheight = renderTexture.height;
+
+        tex = new Texture2D(texwidth, texheight, TextureFormat.RGB24, false);
+        tex.ReadPixels(new Rect(0, 0, texwidth, texheight), 0, 0);
+
+        RenderTexture.active = currentRT;
+        return tex;
+    }
+
+
+    public void SaveImage(string path)
+    {
+        Texture2D img = GetImage();
+        byte[] bytes = img.EncodeToPNG();
+        System.IO.File.WriteAllBytes(path, bytes);
+    }
+
+    public void Update()
+    {		
+        SaveImage("../data/test.png");
+    }
+}
